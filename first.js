53244ed8357be1908a3756f5c237acde043e104b
shreyavaidya3494 <34shreya1994@gmail.com>
2023-11-21 14:26:21 +0530

    checking node

--- first.js
+++ first.js
@@ -1,13 +1,6 @@
-// console log is javascript inbuilt method or function.
-// () means paranthesis
-// There are 2 types of functions- inbuilt functions (already present or default) or customer functions (made by us)
-// console log is a reserved keyword and inbuilt one so compiler goes on its own and executes the function
-// console log gets called on its own because its an inbuilt function
-// variable is a container which is used to store data 
-// Alphabet or alpha-numeric is string data type and is written in quotes. Eg- console.log('shreya123')
-// Data type means the type of data-String, boolean , int, float etc
-console.log('apple')
-console.log('mango')
-console.log('banana')
-console.log('grapes')
-console.log('pear')
+console.log('apple');
+console.log('mango');
+console.log('banana');
+console.log('grapes');
+console.log('pear');
+
--- theory.js
+++ theory.js
@@ -2,3 +2,14 @@
 // Jump from one website to other
 // Hide and show some part of screen-toggle efect
 // Using HTML, we create webpage and using CSS we beautify it but to create website and make it clickable we need javascript, typescript, angular
+
+// console log is javascript inbuilt method or function.
+// () means paranthesis
+// There are 2 types of functions- inbuilt functions (already present or default) or customer functions (made by us)
+// console log is a reserved keyword and inbuilt one so compiler goes on its own and executes the function
+// console log gets called on its own because its an inbuilt function
+// variable is a container which is used to store data 
+// Alphabet or alpha-numeric is string data type and is written in quotes. Eg- console.log('shreya123')
+// Data type means the type of data-String, boolean , int, float
+
+// To run this, we go to terminal and write node file name enter
