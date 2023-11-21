36b79837504c8c34e54c12b72634620c8091748a
shreyavaidya3494 <34shreya1994@gmail.com>
2023-11-21 14:17:36 +0530

    changes in first.js

---
+++ theory.js
@@ -0,0 +1,4 @@
+// To make the webpage clickable, we need javascript
+// Jump from one website to other
+// Hide and show some part of screen-toggle efect
+// Using HTML, we create webpage and using CSS we beautify it but to create website and make it clickable we need javascript, typescript, angular
