5a73a6a68ef9da346360f8b341db41b654cd6231
shreyavaidya3494 <34shreya1994@gmail.com>
2023-11-22 11:57:29 +0530

    datatype file

---
+++ Javacript folder/21Nov/dataTypes.js
@@ -0,0 +1,39 @@
+console.log("testing html js linkage to see this command in html output screen upon clicking on inspect and console");
+
+// Datatypes in Javascript- Primitive and non-Primitive
+// 1) Primitive-Means single value holder
+// -Number->Takes whole number or decimal number.
+// -String-> Take alphabets or alphanumeric values.Always in double or single quotes
+// -Boolean
+// -null
+// -undefined
+// -Symbol
+
+// 2)Non-Primitive- means multi value holder
+// -Object
+
+// typeof is an operator which is used to get data type of a variable
+
+
+let amount=95.8;
+typeof(amount);
+console.log("Data type of amount is ", typeof(amount));
+
+let x=4;
+console.log("Data type of x is ", typeof(x));
+
+let surname="Vaidya"
+console.log("Data type of surname is ", typeof(surname));
+
+let surname2="Tambe3"
+console.log("Data type of surname2 is ", typeof(surname2));
+
+
+let answer1=true;
+console.log("Data type of answer1 is ", typeof(answer1));
+
+let answer2=false;
+console.log("Data type of answer2 is ", typeof(answer2));
+
+let word='3456';
+console.log("Data type of word is ", typeof(word)); //String because it is in souble quotes
---
+++ Javacript folder/21Nov/variable.js
@@ -0,0 +1,8 @@
+// Variables
+
+let name;
+console.log("1-let name", name);
+
+let names="shreya"
+console.log("2-let name shreya check", names);
+
