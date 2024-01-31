6cf1cb120dd6cc5551634d76aaa81644b8b86bfb
shreyavaidya3494 <34shreya1994@gmail.com>
2024-01-31 23:48:17 +0530

    Cypress 28Jan implict assertion

--- Cypress/Jan28/cypress/e2e/spec.cy.js
+++ Cypress/Jan28/cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
-describe("registration test", function()
+describe("registration test", ()=>
 {
-  it("username test", function()
+  it("username test", ()=>
   {
     cy.visit("http://127.0.0.1:5500/Cypress/Jan28/index.html")
     cy.get("button").click()
@@ -8,29 +8,58 @@ describe("registration test", function()
     cy.get('.inp_2').type("123456789")
     cy.get('#sub_btn').click()
   })
-
+})
 
 //implicit assertions
 //1. should
 //2. and
-
-it("test- implicit assertion", ()=>
+describe("implicit assertion testing for index html", ()=>
+{
+//case1: All conditions true- then final result of testing is pass
+it("test index.html- implicit assertion", ()=>
 {
  cy.visit("http://127.0.0.1:5500/Cypress/Jan28/index.html")
- cy.url().should('eq',"http://127.0.0.1:5500/Cypress/Jan28/index.html")
- cy.url().should('not.eq',"http://127.0.0.1:5500/Cypress/Jan28/index.html")
- cy.url().should('contain',"index")
- cy.url().should('include',"5000")
+ cy.url().should("eq","http://127.0.0.1:5500/Cypress/Jan28/index.html")
+ cy.url().should("not.eq","http://127.0.0.1:55900/Cypress/Jan28/index.html")
+ cy.url().should("contain","index")
+ cy.url().should("include","5500")
+})
+})
 
   //or
   //this can be done like this also
-  cy.visit("http://127.0.0.1:5500/Cypress/Jan28/index.html")
-  cy.url().should('eq',"http://127.0.0.1:5500/Cypress/Jan28/index.html")
-  .should('not.eq',"http://127.0.0.1:5500/Cypress/Jan28/index.html")
-  .should('contain',"index")
-  .should('include',"5000")
+  describe("myHomeProject",()=>
+  {
+  it("myhome website implicit assertion",()=>
+  //case2: All conditions true, one false- then final result of testing is fail, .should.should
+  {
+  cy.visit("https://www.myhomeconstructions.com/")
+  cy.url().should('eq',"https://www.myhomeconstructions.com/")
+  .should('not.eq',"navadweepa")
+  .should('contain',"house")
+  .should('include',"constructions")
+  })
+}
+  )
+//case3: All conditions true- then final result of testing is pass-.should.and
+describe("Tatamotors test suit" ,()=>
+{
+it("Tatamotors implict test1",()=>
+{
+  cy.visit("https://www.tatamotors.com/")
+  cy.url().should('eq',"https://www.tatamotors.com/")
+  .and('not.eq',"https://www.google.com/")
+  .and('contain','bakery')
+  .and('include','tatamotors')
+})
+it("Tatamotors implict test2",()=>
+{
+  cy.visit("https://www.tatamotors.com/")
+  cy.url().should('contain',"www")
+}
+)
 
 //can use and also instead of should
 })
 
-})
+
