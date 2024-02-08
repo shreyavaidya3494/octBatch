7300d2924134dd82c6072f2743de18fe857fc7ec
shreyavaidya3494 <34shreya1994@gmail.com>
2024-02-08 19:43:12 +0530

    Template driven form

---
+++ Angular_8Jan/twentyFourDec copy/src/app/user/user-login/user-login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent {
+login(data:any)
+{
+  console.log(data)
+}
+}
