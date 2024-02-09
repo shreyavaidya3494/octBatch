a656f6f1867fff04f00016a8edb9959b213b61b1
shreyavaidya3494 <34shreya1994@gmail.com>
2024-02-09 23:42:13 +0530

    Angular-9Jan-Template driven form- input field error message, patterns, default, bootstrap install and intro

---
+++ Angular_9Jan/9Jan/src/app/user/user-login/user-login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent {
+  name="Vaidya"
+login(data:any)
+{
+  console.log(data)
+}
+}
