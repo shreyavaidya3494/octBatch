ff93f7a104d5041eda3229e4d2587a3ab7cedbbd
shreyavaidya3494 <34shreya1994@gmail.com>
2024-02-20 16:34:53 +0530

    Angular-18Jan-Default values in form fields, extra white space removal in fields, enabling/disabling submit button, checkbox and white space validator

---
+++ Angular_18Jan/18Jan/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { TestComponent } from './test/test.component';
+import { DirectiveComponent } from './directive/directive.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+console.log('app module calling...............');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DatabindingComponent,
+    TestComponent,
+    DirectiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
